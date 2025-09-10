f9d7cfc3840ffb1cbb05d81899b94b9e9cdf8e95
BassaniDavid <davbassan98@gmail.com>
2025-09-10 15:37:15 +0200

    feat: homepage first steps with animated svg

--- japan travel/components/Header.jsx
+++ japan travel/components/Header.jsx
@@ -1,15 +1,15 @@
+import { NavLink } from "react-router-dom";
+
 const Header = () => {
   return (
-    <header className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid d-flex justify-content-around">
-        <a className="header-logo" href="/"></a>
-        <a href="/scopri-giappone" className="">
-          scopri il Giappone
-        </a>
-        <a href="/organizza-viaggio" className="">
-          programma il tuo viaggio
-        </a>
-      </div>
+    <header className="container-fluid d-flex justify-content-around align-items-center mb-5 shadow sticky-top">
+      <NavLink className="header-logo" to="/"></NavLink>
+      <NavLink to="/scopri-giappone" className="">
+        scopri il Giappone
+      </NavLink>
+      <NavLink to="/organizza-viaggio" className="">
+        programma il tuo viaggio
+      </NavLink>
     </header>
   );
 };
--- japan travel/pages/Homepage.jsx
+++ japan travel/pages/Homepage.jsx
@@ -1,7 +1,76 @@
+import { NavLink, Link } from "react-router-dom";
+
 const Homepage = () => {
   return (
     <main>
-      <img src="../img/logo-1.png" alt="" />
+      <section className="homepage DiscoverJapanPage">
+        <div className=" p-5">
+          <Link to="/">
+            <h1 className="mb-5">scopri il Giappone</h1>
+            <p>
+              Il Giappone è una terra di contrasti affascinanti, dove l'antica
+              tradizione dei templi e dei giardini zen si fonde con la modernità
+              frenetica delle metropoli. Dai ciliegi in fiore ai grattacieli
+              futuristici, ogni dettaglio racconta una storia unica.
+              <br />
+              <span className="fw-bold">Per saperne di più</span>
+            </p>
+          </Link>
+        </div>
+        <img
+          id="flower-1"
+          className="flower"
+          src="../img/homepage/flower-1.svg"
+          alt="flower"
+        />
+        <img
+          id="flower-2"
+          className="flower"
+          src="../img/homepage/flower-2.svg"
+          alt="flower"
+        />
+        <img className="" src="../img/homepage/japan.webp" alt="" />
+      </section>
+      <section className="homepage PlanYourTripPage">
+        <img className="" src="../img/homepage/japan.webp" alt="" />
+        <div className=" p-5">
+          <Link to="/">
+            <h1 className="mb-5">Programma il tuo viaggio</h1>
+            <p>
+              Pronto a partire per il Giappone? Qui troverai tutti gli strumenti
+              e le informazioni necessarie per pianificare il tuo itinerario
+              ideale. Dalle mete imperdibili ai consigli pratici, ogni dettaglio
+              è pensato per rendere la tua esperienza indimenticabile.
+              <br />
+              <span className="fw-bold">Per saperne di più</span>
+            </p>
+          </Link>
+        </div>
+        <img
+          id="airplane"
+          src="../img/homepage/airplane-3.svg"
+          alt="airplane"
+        />
+        <img
+          id="cloud-1"
+          className="cloud"
+          src="../img/homepage/cloud-1.svg"
+          alt="cloud"
+        />
+        <img
+          id="cloud-2"
+          className="cloud"
+          src="../img/homepage/cloud-1.svg"
+          alt="cloud"
+        />
+        <img
+          id="cloud-3"
+          className="cloud"
+          src="../img/homepage/cloud-1.svg"
+          alt="cloud"
+        />
+      </section>
+      <section className="homepage RegionPage"></section>
     </main>
   );
 };
