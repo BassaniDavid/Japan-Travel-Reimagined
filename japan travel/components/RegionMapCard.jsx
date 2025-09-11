dc2aebf42f71638f86f7d0f50c120721248d4e63
BassaniDavid <davbassan98@gmail.com>
2025-09-11 13:41:50 +0200

    fix: map bug on kansai-2

--- japan travel/components/RegionMapCard.jsx
+++ japan travel/components/RegionMapCard.jsx
@@ -1,21 +1,25 @@
+import { Link } from "react-router-dom";
+
 const RegionMapCard = ({ details, onPrevClick, onNextClick }) => {
   return (
-    <div className="region-card">
-      <div className="card-controls">
-        <h2>esplora le regioni</h2>
-      </div>
-
-      {details ? (
-        <div>
-          <button onClick={onPrevClick}>&lt;</button>
-          <img src={details.image} alt="" />
-          <button onClick={onNextClick}>&gt;</button>
-          <p>{details.description}</p>
+    <Link to={`/regioni/${details.title}`}>
+      <div className="region-card">
+        <div className="card-controls">
+          <h2>esplora le regioni</h2>
         </div>
-      ) : (
-        <p>Passa il mouse su una regione per scoprire di più</p>
-      )}
-    </div>
+
+        {details ? (
+          <div>
+            <button onClick={onPrevClick}>&lt;</button>
+            <img src={details.image} alt="" />
+            <button onClick={onNextClick}>&gt;</button>
+            <p>{details.description}</p>
+          </div>
+        ) : (
+          <p>Passa il mouse su una regione per scoprire di più</p>
+        )}
+      </div>
+    </Link>
   );
 };
 
