f4e45f97bb515a54763333baeec75eb7c68bae3f
BassaniDavid <davbassan98@gmail.com>
2025-09-12 17:25:02 +0200

    fix: last route

--- japan travel/components/DiscoverCard.jsx
+++ japan travel/components/DiscoverCard.jsx
@@ -2,7 +2,10 @@ import { Link } from "react-router-dom";
 
 const DiscoverCard = ({ info }) => {
   return (
-    <Link to={info.path} className="discover-card-link">
+    <Link
+      to={`/Japan-Travel-Reimagined${info.path}`}
+      className="discover-card-link"
+    >
       <div className="discover-card">
         <div className="discover-content">
           <h2 className="mb-4">{info.title}</h2>
