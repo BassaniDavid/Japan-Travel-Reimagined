9a8206f03fa236187227efaba04fcf7915a99ad5
BassaniDavid <davbassan98@gmail.com>
2025-09-12 18:06:24 +0200

    last image fix (spero)

--- japan travel/data/homepage/regionMapData.js
+++ japan travel/data/homepage/regionMapData.js
@@ -3,55 +3,55 @@ const regionMapData = {
     title: "Hokkaido",
     description:
       "La terra dei grandi spazi, famosa per la sua natura selvaggia, i paesaggi innevati e il cibo delizioso.",
-    image: "../img/homepage/region/hokkaido.png",
+    image: "/img/homepage/region/hokkaido.png",
   },
   tohoku: {
     title: "Tohoku",
     description:
       "Una regione ricca di storia e tradizioni, con festival unici e panorami naturali incontaminati, che raccontano l'anima profonda del Giappone.",
-    image: "../img/homepage/region/tohoku.png",
+    image: "/img/homepage/region/tohoku.png",
   },
   kanto: {
     title: "Kanto",
     description:
       "Il cuore pulsante del Giappone, dove la futuristica Tokyo si fonde con antiche città, creando un mix di energia, cultura e innovazione.",
-    image: "../img/homepage/region/kanto.png",
+    image: "/img/homepage/region/kanto.png",
   },
   chubu: {
     title: "Chubu",
     description:
       "Dominata dalle Alpi Giapponesi, un paradiso per gli amanti della natura e dello sci, con villaggi storici e paesaggi montani mozzafiato.",
-    image: "../img/homepage/region/chubu.png",
+    image: "/img/homepage/region/chubu.png",
   },
   kansai: {
     title: "Kansai",
     description:
       "La culla della cultura giapponese, che ospita tesori secolari come Kyoto, la vibrante Osaka e la storica Nara, con un'offerta gastronomica leggendaria.",
-    image: "../img/homepage/region/kansai.png",
+    image: "/img/homepage/region/kansai.png",
   },
   chugoku: {
     title: "Chugoku",
     description:
       "Una regione di contrasti, dalle coste serenamente pittoresche del Mare Interno di Seto, alle città storiche e ai luoghi della memoria.",
-    image: "../img/homepage/region/chugoku.png",
+    image: "/img/homepage/region/chugoku.png",
   },
   shikoku: {
     title: "Shikoku",
     description:
       "L'isola dello spirito, famosa per il suo millenario pellegrinaggio degli 88 templi, valli remote e paesaggi costieri che invitano alla meditazione.",
-    image: "../img/homepage/region/shikoku.png",
+    image: "/img/homepage/region/shikoku.png",
   },
   kyushu: {
     title: "Kyushu",
     description:
       "La porta tropicale del Giappone, un'isola vulcanica con sorgenti termali, città vivaci e una ricca storia legata alle prime influenze occidentali.",
-    image: "../img/homepage/region/kyushu.png",
+    image: "/img/homepage/region/kyushu.png",
   },
   okinawa: {
     title: "Okinawa",
     description:
       "Un arcipelago paradisiaco di spiagge di sabbia bianca, acque cristalline e una cultura unica, influenzata dal clima subtropicale e dalla storia Ryukyu.",
-    image: "../img/homepage/region/okinawa.png",
+    image: "/img/homepage/region/okinawa.png",
   },
 };
 
