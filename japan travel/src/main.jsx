09a9c0e37b27434768698f37bf36b2964d228b3a
BassaniDavid <davbassan98@gmail.com>
2025-09-13 12:50:43 +0200

    feat: trying to solve the problem of pages not fundwhen refresh certain pages 1

--- japan travel/src/main.jsx
+++ japan travel/src/main.jsx
@@ -4,9 +4,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import App from "./App.jsx";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/Japan-Travel-Reimagined">
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
