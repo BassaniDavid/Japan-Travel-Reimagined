c787327332e72eba881282aaa823f8f000ce18ff
BassaniDavid <davbassan98@gmail.com>
2025-09-24 16:51:05 +0200

    feat: regionPage v2

--- japan travel/pages/RegionPage.jsx
+++ japan travel/pages/RegionPage.jsx
@@ -1,4 +1,5 @@
 import { useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import hokkaido from "../data/region/hokkaido";
 import tohoku from "../data/region/tohoku";
@@ -11,6 +12,8 @@ import kyushu from "../data/region/kyushu";
 import Okinawa from "../data/region/Okinawa";
 
 const RegionPage = () => {
+  const navigate = useNavigate();
+
   const pageData = {
     "/hokkaido": hokkaido,
     "/tohoku": tohoku,
@@ -32,22 +35,27 @@ const RegionPage = () => {
   console.log(data);
 
   return (
-    <main className="container">
+    <main className="container regionPage">
+      <h1>{location.pathname.split("/").pop().toUpperCase()}</h1>
       {data && data.length > 0 ? (
         data.map((sezione, index) => (
-          <section key={index} style={{ marginBottom: "2rem" }}>
+          <section key={index} className="mb-5">
             <h2>{sezione.categoria}</h2>
 
             {/* Contenuto semplice */}
             {Array.isArray(sezione.contenuto) &&
               sezione.contenuto.map((item, i) =>
                 typeof item === "string" ? (
-                  <p key={i}>{item}</p>
+                  <ul className="fs-4">
+                    <li className="mb-1" key={i}>
+                      {item}
+                    </li>
+                  </ul>
                 ) : (
                   <div key={i} style={{ marginLeft: "1rem" }}>
                     <h3 className="fs-1">{item.nome}</h3>
-                    <p className="fs-2">{item.descrizione}</p>
-                    <ul className="fs-3">
+                    <p className="fs-3 mb-1">{item.descrizione}</p>
+                    <ul className="fs-4">
                       {item.puntiInteresse.map((p, j) => (
                         <li key={j}>{p}</li>
                       ))}
@@ -73,6 +81,14 @@ const RegionPage = () => {
       ) : (
         <p>Nessun dato disponibile</p>
       )}
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+        className="px-auto py-2 mb-5 bg-blue-600 text-white rounded-lg hover:bg-blue-700"
+      >
+        Torna indietro
+      </button>
     </main>
   );
 };
